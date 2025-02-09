6bcfea856d8a019cb80c5584894d8a3021a58b1a
fu1999qq <193125826+fu1999qq@users.noreply.github.com>
2025-02-08 21:38:12 -0500

    Full attendance: module_9233

--- src/module_6.py
+++ src/module_6.py
@@ -1,6 +1,5 @@
-def main():
-    print('Hello World')
+import math
 
-if __name__ == '__main__':
-    main()
-// Updated at 2025-02-07 15:02:41
+def calculate_circle_area(radius):
+    return math.pi * radius ** 2
+// Updated at 2025-02-08 21:38:12
