410cdde4f63f7049f1509a726b9d2bdaa86f50ef
fu1999qq <193125826+fu1999qq@users.noreply.github.com>
2025-06-24 16:20:58 -0400

    Full attendance: module_204

--- src/module_3.py
+++ src/module_3.py
@@ -1,6 +1,7 @@
-def main():
-    print('Hello World')
+class DataProcessor:
+    def __init__(self, data):
+        self.data = data
 
-if __name__ == '__main__':
-    main()
-// Updated at 2025-06-16 13:51:55
+    def process(self):
+        return sorted(self.data)
+// Updated at 2025-06-24 16:20:58
