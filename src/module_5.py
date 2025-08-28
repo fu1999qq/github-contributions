dbd113a844550ef1e01b89e1345329414d358c2c
fu1999qq <193125826+fu1999qq@users.noreply.github.com>
2025-08-27 22:58:14 -0400

    Full attendance: module_6602

--- src/module_5.py
+++ src/module_5.py
@@ -1,6 +1,7 @@
-def main():
-    print('Hello World')
+class DataProcessor:
+    def __init__(self, data):
+        self.data = data
 
-if __name__ == '__main__':
-    main()
-// Updated at 2025-08-24 18:55:19
+    def process(self):
+        return sorted(self.data)
+// Updated at 2025-08-27 22:58:14
