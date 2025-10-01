816c5de0063b19772cc7d16cf45b954d87100077
fu1999qq <193125826+fu1999qq@users.noreply.github.com>
2025-10-01 16:29:17 -0400

    Full attendance: module_5268

--- src/module_1.js
+++ src/module_1.js
@@ -1,4 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('App started');
-});
-// Updated at 2025-10-01 10:01:39
+const add = (a, b) => a + b;
+console.log(add(5, 10));
+// Updated at 2025-10-01 16:29:17
